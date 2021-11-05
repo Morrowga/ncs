9faa90b6d5b54ece12bb389a08edfb68821c591a
myatkohein <myatkohein@htut.com>
2021-11-05 16:32:21 +0630

    bo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,8 +40,8 @@
     <link rel="stylesheet" href="{{ asset('web/css/app-light.css') }}" id="lightTheme">
 
     {{-- excel --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     {{--
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4/css/bootstrap.min.css" />
     --}}
