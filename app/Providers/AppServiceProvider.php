eeef455e69911bd1e5cd268c1eb4f7d24578e8f6
Myat Ko Hein <myatkohein@htut.com>
2022-02-03 10:15:45 +0630

    add column to raw articles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $count_raw_article = RawArticle::where('sent_status', 0)->count();
-        // $count_sent_lotaya = RawArticle::where('sent_status', 1)->count();
-        // $count_reject_article = RawArticle::where('sent_status', '>', 1)->count();
-        // view()->share('count_raw_article', $count_raw_article);
-        // view()->share('count_sent_lotaya', $count_sent_lotaya);
-        // view()->share('count_reject_article', $count_reject_article);
+        $count_raw_article = RawArticle::where('sent_status', 0)->count();
+        $count_sent_lotaya = RawArticle::where('sent_status', 1)->count();
+        $count_reject_article = RawArticle::where('sent_status', '>', 1)->count();
+        view()->share('count_raw_article', $count_raw_article);
+        view()->share('count_sent_lotaya', $count_sent_lotaya);
+        view()->share('count_reject_article', $count_reject_article);
     }
 }
